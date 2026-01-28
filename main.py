e54e4aae7fef449a8a914e32c3193e054612daff
aozodbek2197 <aozodbek2197@gmail.com>
2026-01-28 20:52:07 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# 1-masala
+matn = ("Salom Dunyo")
+print(matn.lower())
+# 2-masala
+matn = ("python")
+print(matn.upper())
+# 3-masala
+matn = ("dasturlash")
+print(matn.capitalize())
+# 4-masala
+matn = ("python string metodlari")
+print(matn.title())
+# 5-masala
+matn = (" salom ")
+print(matn.strip())
+# 6-masala
+matn = ("Men Python o'rganaman")
+print(matn.replace("Python", "Django"))
+# 7-masala
+matn = ("olma banan anor")
+print(matn.split())
+# 8-masala
+roy = (["Men", "Pyhton", "bilaman"])
+print(" ".join(roy))
